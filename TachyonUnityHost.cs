3dcba276c18692802b100d24d22f2f01cca60ce9
Sebastiaan Fehr <seb@animaginedreality.com>
2020-02-25 02:31:27 +1000

    Tachyon Unity Client published as an integrated unity package library.

---
+++ TachyonUnityHost.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TachyonCommon;
+using TachyonServerRPC;
+using UnityEngine;
+
+namespace TachyonServerCore {
+    public class TachyonUnityHost : MonoBehaviour {
+
+        public event Action<ClientConnection> OnClientConnected;
+        public event Action<ClientConnection> OnClientDisconnected;
+    
+        private HostCore _host;
+
+        private Queue<ClientConnection> _connections = new Queue<ClientConnection>();
+        private Queue<ClientConnection> _disconnections = new Queue<ClientConnection>();
+        public bool Started { get; set; }
+
+        public void Initialize<TService>(
+            ISerializer serializer, 
+            params TService[] services
+        ) where TService : class {
+            var endPointMap = new EndPointMap(serializer);
+            _host = new HostCore(endPointMap);
+            _host.OnConnected += (c) => _connections.Enqueue(c);
+            _host.OnDisconnected += (c) => _disconnections.Enqueue(c);
+            _host.OnStarted += () => Started = true;
+
+            foreach (TService service in services) 
+                _host.Bind(service);
+
+            _host.Start();
+        }
+
+        // Syncronise threaded events with unity main thread.
+        IEnumerator Start() {
+            while (true) {
+                
+                if (_connections.Any()) {
+                    var connection = _connections.Dequeue();
+                    OnClientConnected?.Invoke(connection);
+                }
+
+                if (_disconnections.Any()) {
+                    var disconnection = _disconnections.Dequeue();
+                    OnClientDisconnected?.Invoke(disconnection);
+                }
+
+                yield return null;
+            }
+        }
+        
+    }
+}
---
+++ UnityHostBindingExt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using TachyonCommon;
+using TachyonServerCore;
+
+public static class UnityHostBindingExt 
+{
+    public static void Bind<TService>(this HostCore host, TService serviceToBind) where TService : class {
+        
+        var serviceTypes = typeof(TService).Assembly.GetTypes();
+        var serviceType = serviceTypes
+            .First(t => {
+                    
+                    var bindingAttr = Attribute.GetCustomAttribute(t,
+                        typeof(HostBindingAttribute));
+                    if (bindingAttr is HostBindingAttribute binding)
+                        return binding.ServiceType == typeof(TService);
+                    
+                    return false;
+                }
+            );
+        
+        var bindMethod = serviceType.GetMethod("Bind");
+        var binderInstance = Activator.CreateInstance(serviceType);
+        bindMethod?.Invoke(binderInstance, new object[] {host, serviceToBind, host._endPoints});
+        
+    }
+}
